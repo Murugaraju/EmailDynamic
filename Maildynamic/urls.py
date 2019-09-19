734a280373a01bb4dd809f54506026df228c50f6
Murugaraju <murugan.iiit2326@gmail.com>
2019-09-20 00:30:25 +0530

    making some backup

--- Maildynamic/urls.py
+++ Maildynamic/urls.py
@@ -20,5 +20,6 @@ from api.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('alertmail/',AlertMailList.as_view()),
-    path('alertmail/<int:pk>',AlertMailDetail.as_view())
+    path('alertmail/<int:pk>',AlertMailDetail.as_view()),
+    path('alertmail/test',test_alertmail)
 ]
--- api/views.py
+++ api/views.py
@@ -5,7 +5,12 @@ from django.http import Http404
 from .models import *
 from .serializers import *
 from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
+
+# For mail related
+from django.core import mail
+from django.core.mail.backends.smtp import EmailBackend
 
 
 class AlertMailList(APIView):
@@ -57,3 +62,53 @@ class AlertMailDetail(APIView):
         alertmailob.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
+@api_view(['post'])
+def test_alertmail(request):
+    print("came in test_alertmail")
+    if len(AlertMailModel.objects.all()) == 0:
+        return Response({"SMPT settings record not exist" :["Try record smpt and reciepient details"]})
+    return Response("working")
+
+def send_mail(subject, contact_list, body):
+
+    try:
+        mailob=AlertMailModel.objects.first()
+        
+        con = mail.get_connection(host=mailob.host_smtpaddress,port=587,fail_silently=False)
+        con.open()
+        print('Django connected to the SMTP server')
+        
+
+
+        host = 'smtp.gmail.com'
+        host_user = 'info.temporary.dev@gmail.com'
+        host_pass = 'demo123456'
+        host_port = 587
+
+        mail_obj = EmailBackend(
+            host=host,
+            port=host_port,
+            password=host_pass,
+            username=host_user,
+            use_tls=True,
+            timeout=10
+        )
+
+        msg = mail.EmailMessage(
+            subject=subject,
+            body=body,
+            from_email=host_user,
+            to=[contact_list],
+            connection=con,
+        )
+        mail_obj.send_messages([msg])
+        print('Message has been sent.')
+
+        mail_obj.close()
+        print('SMTP server closed')
+        return True
+
+    except Exception as _error:
+        print('Error in sending mail >> {}'.format(_error))
+        return False
